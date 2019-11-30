d6b8aa22e4461956b7b4c324856f11c7c698b470
rusweek <rusweek@gmail.com>
2019-11-30 20:31:09 +0700

    написан счетчик как пример работы redux

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {createStore} from 'redux';
 
 const reduser = (state = 0, action) => {
 
@@ -5,18 +6,27 @@ const reduser = (state = 0, action) => {
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'DEC':
+            return state - 1;
         default:
             return state;
     }
 
 };
 
-let state = reduser(undefined, {});
+const  store = createStore(reduser);
 
-state = reduser(state, {type: 'INC'});
+document.getElementById('inc').addEventListener('click', (e)=>{
+    store.dispatch({type: 'INC'});
+});
 
-console.log(state);
+document.getElementById('dec').addEventListener('click', (e)=>{
+    store.dispatch({type: 'DEC'});
+});
 
-state = reduser(state, {type: 'INC'});
+const update = () => {
+    document.getElementById('counter').innerHTML = store.getState();
+};
 
-console.log(state);
+store.subscribe(update);
+
